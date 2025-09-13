27fe08d1bd8eee5d78ac1c27e1f41b9b7ec20c6a
Schoa <phungminhkhoa2006@gmail.com>
2025-09-13 11:58:51 +0700

    13/09
    Variable and Function

---
+++ 13-09/testing.py
@@ -0,0 +1,10 @@
+"""
+Nhiệm vụ 2: Hoàn thành dòng code dưới
+"""
+
+x = 3
+
+def plus():
+    y = 5
+
+# In ra tổng của x và y: 8
---
+++ 13-09/var_and_function.py
@@ -0,0 +1,48 @@
+# Xác định một function
+def a(): # Tạo một function, cần bao gồm : /def/ /tên()/ /:/
+    x = 30 # Local Variable 
+    print(x)
+
+x = 50 # Gloabl Variable
+
+# Giao function cho variable
+var = a
+
+# Gọi function thông qua variable
+print(x)
+a()
+
+""" Syntax để tạo một function
+# Xác định một function
+def function()
+    # Thân của function
+
+# Bỏ qua
+
+# Giao function cho variable
+var = function
+
+# Gọi một function thông qua variable
+function()
+
+!!!
+/function/ có thể thay bằng một cái tên khác
+Khi mà lùi code -> Dùng Tab chứ không dùng Space
+"""
+
+"""
+Nhiệm vụ 1: Tạo function để print ra "Python is hard"
+"""
+
+""" Global vaf Local Variable
+Global Variable thì dùng được trong local function
+Local Variable thì không dùng được ngoài Gloabal
+
+!!!
+Nếu trong Local có variable trùng tên với một global variable, thì trong Local đấy sẽ chấp nhận cái variable local
+"""
+
+"""
+Nhiệm vụ 2: Tạo một function dùng Gloabal Variable
+"""
+
